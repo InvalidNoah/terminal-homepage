817cc6c7ddaa7aa1ce524faf85f59646805f24ec
Noah <84711108+InvalidNoah@users.noreply.github.com>
2023-09-07 18:58:33 +0200

    termianl change

--- src/config/terminal.ts
+++ src/config/terminal.ts
@@ -1,12 +1,12 @@
-export const HOST = 'visitor@localhost';
+export const HOST = 'visitor@noez';
 export const DEFAULT_USER = HOST.split('@')[0];
 export const HOSTNAME = HOST.split('@')[1];
-export const PATHNAME = '/home/webserver/homepage';
+export const PATHNAME = '/home/visitor/';
 
 export const GITHUB_PAGE = 'https://github.mxgnus.de';
-export const GITHUB_USERNAME = 'mxgnus-de';
+export const GITHUB_USERNAME = 'InvalidNoah';
 export const DISCORD_PROFILE = 'https://discord.mxgnus.de';
-export const EMAIL = 'kontakt@mxgnus.de';
+export const EMAIL = 'me@eynoah.de';
 
 export const BANNER = `
  ███▄ ▄███▓▒██   ██▒  ▄████  ███▄    █  █    ██   ██████ 
